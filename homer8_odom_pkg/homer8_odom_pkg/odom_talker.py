0bba79b77df153857c0c94f08b36ebd0ee556e6f
Lin Zhang <linzhank@gmail.com>
2025-04-02 13:45:03 -0500

    add odom

--- homer8_odom_pkg/homer8_odom_pkg/odom_talker.py
+++ homer8_odom_pkg/homer8_odom_pkg/odom_talker.py
@@ -1,34 +1,109 @@
-from serial import Serial
-
 import rclpy
 from rclpy.node import Node
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import Twist, TransformStamped
+from nav_msgs.msg import Odometry
 
-from geometry_msgs.msg import Twist
-
-from math import pi
+from serial import Serial
+from tf_transformations import quaternion_from_euler
+from math import sin, cos, pi
 
 
 class Homer8OdomNode(Node):
     def __init__(self):
         super().__init__("homer8_odom_talker_node")
-        # Config serial port
+        # Init serial port
         self.pico_messenger = Serial("/dev/ttyACM0", 115200)
-        self.pico_receiver_timer = self.create_timer(0.015, self.receive_pico_message)
+        self.pico_interact_timer = self.create_timer(0.02, self.talk_listen_pico)
         self.real_lin_vel = 0.0
         self.real_ang_vel = 0.0
-        # Config publishers and subscribers
+        # Init \cmd_vel subscriber
+        self.target_vel_subscriber = self.create_subscription(
+            topic="cmd_vel",
+            msg_type=Twist,
+            callback=self.send_pico_target_vel,
+            qos_profile=1,
+        )
+        ### START CODEING HERE ### ~? lines
+        # Init \odom publisher and tf broadcaster `odom` -> `base_link`
+        self.odom_publisher = self.create_publisher(
+            topic="odom",
+            msg_type=Odometry,
+            qos_profile=1,
+        )
+        self.odom_base_broadcaster = TransformBroadcaster(self)
+        self.odom_talker_timer = self.create_timer(0.02, self.announce_odometry)  # 50Hz
+        ### END CODING HERE ###
+        self.x = 0.0  # in odom frame
+        self.y = 0.0
+        self.z = 0.033
+        self.th = 0.0
+        self.prev_ts = self.get_clock().now()
+        self.curr_ts = self.get_clock().now()
+        self.get_logger().info("HomeR is awaken.")
 
-    def receive_pico_message(self):
+    def talk_listen_pico(self):
         if self.pico_messenger.inWaiting() > 0:
             real_vels = (
                 self.pico_messenger.readline().decode("utf-8").rstrip().split(",")
             )
             if len(real_vels) == 2:
-                self.real_lin_vel = real_vels[0]
-                self.real_ang_vel = real_vels[1]
-            self.get_logger().debug(
-                f"actual velocity measured \n---\nlinear: {self.real_lin_vel}, angular: {self.real_ang_vel}"
-            )
+                self.real_lin_vel = float(real_vels[0])
+                self.real_ang_vel = float(real_vels[1])
+                self.get_logger().info(
+                    f"actual velocity measured \n---\nlinear: {self.real_lin_vel}, angular: {self.real_ang_vel}"
+                )
+            else:
+                self.get_logger().warn("Pico message decoding ABNORMAL!")
+
+    def send_pico_target_vel(self, cmd_vel_msg):
+        targ_linv = cmd_vel_msg.linear.x
+        targ_angv = cmd_vel_msg.angular.z
+        self.pico_messenger.write(f"{targ_linv},{targ_angv}\n".encode("utf-8"))
+        self.get_logger().info(
+            f"target velocity set \n---\nlinear: {targ_linv}, angular: {targ_angv}"
+        )
+
+    def announce_odometry(self):
+        # Calculate time and pose change
+        self.curr_ts = self.get_clock().now()
+        dt = (self.curr_ts - self.prev_ts).nanoseconds * 1e-9
+        dx = self.real_lin_vel * cos(self.th) * dt
+        dy = self.real_lin_vel * sin(self.th) * dt
+        dth = self.real_ang_vel * dt
+        self.x += dx
+        self.y += dy
+        self.th += dth
+        quat = quaternion_from_euler(0, 0, self.th)
+        self.prev_ts = self.curr_ts  # update time stamp
+        # broadcast `odom` to `base_link` transformation
+        odom_base_tf = TransformStamped()
+        odom_base_tf.header.stamp = self.curr_ts.to_msg()
+        odom_base_tf.header.frame_id = "odom"
+        odom_base_tf.child_frame_id = "base_link"
+        odom_base_tf.transform.translation.x = self.x
+        odom_base_tf.transform.translation.y = self.y
+        odom_base_tf.transform.translation.z = self.z
+        odom_base_tf.transform.rotation.x = quat[0]
+        odom_base_tf.transform.rotation.y = quat[1]
+        odom_base_tf.transform.rotation.z = quat[2]
+        odom_base_tf.transform.rotation.w = quat[3]
+        self.odom_base_broadcaster.sendTransform(odom_base_tf)
+        # publish odom topic
+        odom_msg = Odometry()
+        odom_msg.header.stamp = self.curr_ts.to_msg()
+        odom_msg.header.frame_id = "odom"
+        odom_msg.child_frame_id = "base_link"
+        odom_msg.pose.pose.position.x = self.x
+        odom_msg.pose.pose.position.y = self.y
+        odom_msg.pose.pose.position.z = self.z
+        odom_msg.pose.pose.orientation.x = quat[0]
+        odom_msg.pose.pose.orientation.y = quat[1]
+        odom_msg.pose.pose.orientation.z = quat[2]
+        odom_msg.pose.pose.orientation.w = quat[3]
+        odom_msg.twist.twist.linear.x = self.real_lin_vel
+        odom_msg.twist.twist.angular.z = self.real_ang_vel
+        self.odom_publisher.publish(odom_msg)
 
 
 def main():
